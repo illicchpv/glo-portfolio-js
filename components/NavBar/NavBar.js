de04924a046aed213e0b6c0c04a5dcdacbd02e5a
illicchpv <illich58@mail.ru>
2026-01-11 22:50:10 +0300

    ?nav-bar

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,81 @@
+import {BaseComponent} from '../../components/base/BaseComponent.js';
+
+export class NavBar extends BaseComponent {
+  constructor() {
+    super();
+    this._userName = null;
+  }
+
+  // Указываем, за какими атрибутами следить
+  static get observedAttributes() {
+    return ['user-name'];
+  }
+
+  // Реакция на изменение атрибутов
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'user-name' && oldValue !== newValue) {
+      this._userName = newValue;
+      this._updateUserName();
+    }
+  }
+
+  _updateUserName() {
+    // Проверяем, загружен ли уже HTML
+    const nameSpan = this.querySelector('.nav-bar__title span');
+    if (nameSpan) {
+      nameSpan.textContent = this._userName || 'Гость';
+    }
+  }
+
+  get userName() {
+    return this._userName;
+  }
+
+  set userName(value) {
+    if (value) {
+      this.setAttribute('user-name', value);
+    } else {
+      this.removeAttribute('user-name');
+    }
+  }
+
+  connectedCallback() {
+    this.loadTemplate(import.meta.url);
+  }
+
+  // Вызывается при загрузке шаблона (из BaseComponent)
+  render() {
+    super.render();
+    this._updateUserName();
+    this._setupEventListeners();
+  }
+
+  _setupEventListeners() {
+    const btn = this.querySelector('.my-component__btn');
+    if (btn) {
+      btn.addEventListener('click', () => {
+        this.dispatchEvent(new CustomEvent('nav-bar-action', {
+          detail: {
+            timestamp: new Date().toISOString(),
+            message: `Кнопка нажата пользователем ${this._userName || 'Гость'}`
+          },
+          bubbles: true, // Событие всплывает
+          composed: true // Проходит сквозь Shadow DOM (если бы он был)
+        }));
+      });
+    }
+  }
+}
+customElements.define('nav-bar', NavBar);
+
+/* создание с обработкой сообщений компонента 👇
+<nav-bar>⭐Это контент, попадет в &lt;slot&gt;🙃&lt;/slot&gt;.</nav-bar>
+<script>
+  document.querySelector('nav-bar').addEventListener('nav-bar-action', (event) => {
+    console.log('Событие от nav-bar!', event.detail);
+    alert(event.detail.message);
+  });
+</script>
+*/
+
+
