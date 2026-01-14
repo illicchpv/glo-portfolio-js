dfdbdd3ebea6bf06db2cb980b8d268d0e566c02e
illicchpv <illich58@mail.ru>
2026-01-14 23:02:20 +0300

    осталось создать tg бота и опубликовать на хостинге с php

---
+++ js/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const ajaxSend = async (formData) => { // создаем функцию отправки формы
+  const fetchResp = await fetch("telegram.php", { // указываем обработчик формы — telegram.php
+    method: "POST", // метод, которым мы отправляем форму
+    body: formData, // что будет внутри формы — содержимое input
+  });
+  if (!fetchResp.ok) { // если ошибка, то...
+    throw new Error(`Ошибка по адресу ${url}, статус ошибки ${fetchResp.status}`); // выводим статус ошибки и текст
+  }
+  return await fetchResp.text(); // если все хорошо, возвращаем ответ сервера
+};
+
+let count = 0;
+const countMax = 100;
+function getForms() {
+  count++;
+  if (count > countMax) {
+    console.log('error get forms');
+    return;
+  }
+  const forms = document.querySelectorAll("form"); // находим все теги form
+  if (!forms || forms.length === 0) {
+    console.log(`wait forms ${count}...`);
+    return setTimeout(() => {getForms();}, 100);
+  }
+  console.log('forms ok length:', forms.length);
+  forms.forEach((form) => { // для каждой формы...
+    form.addEventListener("submit", function (e) { // отслеживаем событие отправки
+      e.preventDefault(); // отменить стандартную отправку формы
+      const formData = new FormData(this); // собираем все данные из формы
+
+      // console.log(formData);
+      // debugger;
+
+      ajaxSend(formData) // передаем данные из формы в обработчик
+        .then((response) => { // если все успешно, то..
+          this.innerHTML = "Спасибо,<br> заявку получили"; /* окно благодарности */
+          form.reset(); /*  очищаем поля формы */
+        })
+        .catch((err) => console.error(err)); /* если ошибка, выводим в консоль */
+    });
+  });
+}
+getForms();
